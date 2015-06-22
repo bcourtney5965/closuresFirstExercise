1f02069c420f7eba297d9c9d20560454f0f6a0c6
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-06-22 17:05:34 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Encapsulated a quiz in a function.  Practice for future work with Modules.  6 global variables (before) --> 1 global variable (after).
---
+++ closuresFirstExercise.js
@@ -0,0 +1,36 @@
+// Converting a quiz to one encapsulated by a function
+// 6 global variables --> 1 global variable
+// Preparing for future work with modules
+function quiz() {
+	return function () {
+		var answer1 = "blue",
+		    answer2 = "no",
+		    answer3 = "yes";
+
+		var userAnswer1 = prompt("what color is the sky?")
+		if (userAnswer1 === answer1) {
+
+		    alert("Correct")
+		} else {
+		    alert("Sorry, that was wrong")
+		}
+		var userAnswer2 = prompt("Do cows fly?")
+
+		if (userAnswer2 === answer2) {
+		    alert("Correct")
+		} else {
+		    alert("Sorry, that was wrong")
+		}
+
+		var userAnswer3 = prompt("Do birds fly?")
+
+		if (userAnswer3 === answer3) {
+		    alert("Correct")
+		} else {
+		    alert("Sorry, that was wrong")
+		}
+	}
+}
+var runQuiz = quiz();
+
+runQuiz();
